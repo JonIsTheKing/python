3904b9002be1e1935bdf42cfcd8ca28a9771a23b
JonIsTheKing <57835188+JonIsTheKing@users.noreply.github.com>
2021-09-13 06:22:34 +0530

    Update removeEmptyLines.py

--- removeEmptyLines.py
+++ removeEmptyLines.py
@@ -1,3 +1,5 @@
+# Reads data from trump file, removes the empty lines and writes to donald file.
+
 with open('trump.txt', 'r') as ff:
     with open('donald.txt','w') as fd:
         for line in ff:
